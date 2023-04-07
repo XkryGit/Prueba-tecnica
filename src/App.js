368fefa93c5ad24760cafb45d89a1bc37b6fe4c3
XkryGit <gilabertejecutivo@gmail.com>
2023-04-08 00:04:07 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [productsCopy, setProductsCopy] = useState([]);
+
+  let productsInitial = [];
+
+  function handelProductInitial(x) {
+    productsInitial = [...x];
+  }
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products`)
+      .then((response) => response.json())
+      .then((json) => handelProductInitial(json))
+      .then(() => setProducts(productsInitial))
+      .then(() => setProductsCopy(productsInitial))
+      .catch((error) => alert("Ha ocurrido un error, intentelo más tarde."));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/categories`)
+      .then((response) => response.json())
+      .then((json) => setCategories(json))
+      .catch((error) => alert("Ha ocurrido un error, intentelo más tarde."));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <h1 className="title">Prueba técnica Adolfo Zambrana</h1>
+      <div className="filter-button-container">
+        <button
+          className="filter-button"
+          onClick={() => setProducts(productsCopy)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          ALL CATEGORIES
+        </button>
+        {categories.map((categorie) => (
+          <>
+            <button
+              className="filter-button"
+              onClick={() =>
+                setProducts(
+                  productsCopy.filter(
+                    (product) => product.category === categorie
+                  )
+                )
+              }
+            >
+              {categorie.toUpperCase()}
+            </button>
+          </>
+        ))}
+      </div>
+
+      <div className="product-list-container">
+        <span className="list-header">Image</span>
+        <span className="list-header">Name</span>
+        <span className="list-header">Price</span>
+        {products.map((product) => (
+          <>
+            <img
+              className="list"
+              src={product.image}
+              alt="../public/imgAlt.png"
+            />
+            <h4 className="list">{product.title}</h4>
+            <span className="list">{product.price}€</span>
+          </>
+        ))}
+      </div>
+    </>
   );
 }
 
